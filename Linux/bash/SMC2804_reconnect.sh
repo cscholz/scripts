3948cfc25361b959dfab696261e567e9d484782e
Christian Scholz <github@2nibbles4u.de>
2016-01-18 16:36:59 +0100

    Triggers a remote reconnect on an SMC2804WBR router

---
+++ Linux/bash/SMC2804_reconnect.sh
@@ -0,0 +1,3 @@
+curl "http://192.168.2.1/login.htm" -d "login&page=login&year=107&month=10&day=13&hour=15&minute=48&second=15&pws=PASSWORT"
+curl "http://192.168.2.1/status_main.htm" -d "page=status_main&button=pppoedisconnect&disconnect=Disconnect"
+curl "http://192.168.2.1/status_main.htm" -d "page=status_main&button=pppoeconnect&connect=+Connect+
