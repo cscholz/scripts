f077cea21da14637c486221b6499dd604eea5b48
Christian Scholz <github@2nibbles4u.de>
2016-01-18 16:36:18 +0100

    Script to install security updates auomatically.

---
+++ Linux/bash/apt/ins_secupdates.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Cron Script - run from /etc/crontab or /etc/cron.daily
+#
+# Checks for security updates, install them and send a email notification
+
+MAIL_TO="postmaster@o-o-s.de"
+
+LANG=C
+
+if [[ $1 = "" ]]; then
+        echo "incorrect usage; specify the procress name an the command line"
+else
+
+        # instal security updates automatically
+        if [[ `apt-get update -o Dir::Etc::SourceList=$1 | egrep '(Get|Hole|Ign)'` ]]
+                then
+
+                UPDATES=`apt-get -s -o Dir::Etc::SourceList=$1 upgrade 2>&1 | grep Inst | wc -l`
+
+                if [ $UPDATES -ne 0 ]
+                        then
+                        PACKAGES=`apt-get -s -o Dir::Etc::SourceList=$1 upgrade 2>&1 | grep Inst`
+                        apt-get -y -o Dir::Etc::SourceList=$1 upgrade 2>&1
+                        echo "Security updates have been installed on `hostname`:
+
+                        $PACKAGES
+
+                        Be aware of the updated packages" | mailx -s "$UPDATES security update(s) installed on `hostname`" $MAIL_TO
+                        echo "$UPDATES security update(s) installed on `hostname`";
+                fi
+        fi
+fi
+unset PACKAGES
+unset UPDATES
+
+apt-get update > /dev/null 2>&1
+
+exit 0
+
